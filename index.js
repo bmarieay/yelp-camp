fa723396fea913b4359d2ffb298052b217dbe042
bmarieay <marieantonettebacay@gmail.com>
2022-01-21 20:37:47 -0800

    populated and render views

--- index.js
+++ index.js
@@ -87,7 +87,9 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 //show a single campground
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id)
+        .populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show', {campground});
 }))
 
