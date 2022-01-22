3402fb7602356284951aaea851862cb1117ec6ab
bmarieay <marieantonettebacay@gmail.com>
2022-01-21 18:57:01 -0800

    added a review route associated with a campground

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utility/catchAsync');
 const ExpressError = require('./utility/ExpressError');
 const {campgroundSchema} = require('./schemas')
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const methodOverride = require("method-override");
 const { nextTick } = require("process");
 const port = 3000;
@@ -97,6 +98,16 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not Found', 404))
