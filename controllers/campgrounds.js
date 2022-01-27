0a4acfabd70378a4554ae7117e3029458a8b9dab
bmarieay <marieantonettebacay@gmail.com>
2022-01-27 15:16:05 -0800

    updated img upload for editing

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -48,7 +48,11 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.editCampground = async (req, res) => {
     const {id} = req.params;
+    //make this shorter later
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});//spread each properties
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.images.push(...imgs)
+    await campground.save();
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,7 +23,7 @@ router.route('/:id')
     //show a single campground
     .get(catchAsync(campgrounds.showCampground))
     //add an updated campground to the server
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.editCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.editCampground))
     //delete a campground
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
