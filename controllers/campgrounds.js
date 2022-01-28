30335d27c2e873ab2025a083dff59ae0364eef0b
bmarieay <marieantonettebacay@gmail.com>
2022-01-27 17:58:06 -0800

    updated delete image and basic thumbnail

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,5 @@
 const Campground = require("../models/campground")
-
+const { cloudinary } = require("../cloudinary")
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
@@ -47,12 +47,20 @@ module.exports.renderEditForm = async (req, res) => {
 }
 
 module.exports.editCampground = async (req, res) => {
+    console.log(req.body)
     const {id} = req.params;
     //make this shorter later
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});//spread each properties
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
     campground.images.push(...imgs)
     await campground.save();
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({$pull: {images: {filename: {$in:  req.body.deleteImages}}}})
+        console.log(campground)
+    }
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }
