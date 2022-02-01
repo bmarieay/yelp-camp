d913d9f0f6bac834a6f8b6f80306945886b03b92
bmarieay <marieantonettebacay@gmail.com>
2022-01-31 17:05:09 -0800

    used remote db for seeding

--- seeds/index.js
+++ seeds/index.js
@@ -11,7 +11,8 @@ const key = process.env.API_KEY;
 const Campground = require('../models/campground');
 
 //initial connection error
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl)
     .then(() => {
         console.log('CONNECTION MONGO OPEN!')
     })
